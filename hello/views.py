00810d412a7e0ef908ddcabd8cc13fbaacea259e
Koglersites <k.mario1@gmx.at>
2017-03-07 12:22:22 +0100

    test commit

--- hello/views.py
+++ hello/views.py
@@ -8,7 +8,7 @@ import requests
 def index(request):
     r = requests.get('http://httpbin.org/status/418')
     print r.text
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    return HttpResponse('<pre>' + r.text + ' <br> test </pre>')
 
 
 def db(request):
